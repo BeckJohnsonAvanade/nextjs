cc3e470ea79eb49dd1da0d09a4cab91eccf01ca4
Beck Johnson <beck.johnson@avanade.com>
2023-05-26 11:11:46 -0700

    initial commit, copied from https://github.com/vercel/next-learn/tree/master/basics/data-fetching-starter

---
+++ pages/_app.js
@@ -0,0 +1,41 @@
+import localFont from 'next/font/local';
+
+import '../styles/global.css';
+
+const myFont = localFont({
+  src: [
+    {
+      path: '../public/fonts/Gotham-Book.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Gotham-BookItalic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Gotham-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Gotham-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Gotham-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    }
+  ],
+});
+ 
+export default function App({ Component, pageProps }) {
+  return (
+       <main className={myFont.className}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
